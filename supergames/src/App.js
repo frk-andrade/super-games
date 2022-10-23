7ca55497e762329f74beb16449ada82db292d0d4
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-10-23 00:40:36 -0300

    Add reset css and component nav

--- supergames/src/App.js
+++ supergames/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import './reset.css';
+import Nav from './components/Nav';
+
 
 function App() {
   return (
     <div className="App">
+      <Nav />
     </div>
   );
 }
---
+++ supergames/src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import './Nav.css';
+
+const Nav = () => {
+  return (
+    <nav className='nav'>
+      <img src='../images/logo.svg' alt='Logo'></img>
+      <button>
+        Login
+      </button>
+      <img src='../images/Icone.svg' alt='Buscar'></img>
+    </nav>
+  )
+};
+
+export default Nav;
