0df2819dd38015174ce224f67d29539a29b1eb50
Franklin <frkdj25@gmail.com>
2022-10-24 19:52:52 -0300

    feat: adding console images and creating Consoles folder

---
+++ supergames/src/components/Consoles/index.js
@@ -0,0 +1,11 @@
+import './Consoles.css'
+
+const Consoles = () => {
+    const products = [
+        {
+            img: '../images/game-boy.svg',
+            name: 'Game Boy',
+            price: 'R$ 400,00'
+        },
+    ]
+}
