3ee44db6e01bc4bfff5aa399a46837a39de78394
Franklin <frkdj25@gmail.com>
2022-10-28 17:26:21 -0300

    feature: adding products on the list

---
+++ supergames/src/components/Diversos/Diversos.js
@@ -0,0 +1,37 @@
+import './Diversos.css';
+
+const Diversos = () => {
+
+    const products = [
+        {
+            img: '../images/camisa-atari.svg',
+            name: 'Camisa Atari',
+            price: 'R$ 90,00'
+        },
+        {
+            img: '../images/camisa-snes.svg',
+            name: 'Camisa SNES',
+            price: 'R$ 100,00'
+        },
+        {
+            img: '../images/fantasia-pikachu.svg',
+            name: 'Fantasia Pikachu',
+            price: 'R$ 400,00'
+        },
+        {
+            img: '../images/oculos-rv.svg',
+            name: 'Óculos RV',
+            price: 'R$ 500,00'
+        },
+        {
+            img: '../images/relogio-retro.svg',
+            name: 'Relógio Retrô',
+            price: 'R$ 190,00'
+        },
+        {
+            img: '../images/sonic.svg',
+            name: 'Boneco Sonic',
+            price: 'R$ 350,00'
+        }
+    ]
+}
