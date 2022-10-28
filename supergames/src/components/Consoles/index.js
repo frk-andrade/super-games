a832927025dc73de5a09126d807d2ce6eba24c7d
Franklin <frkdj25@gmail.com>
2022-10-28 17:16:07 -0300

    fix: ajusting layout

--- supergames/src/components/Consoles/index.js
+++ supergames/src/components/Consoles/index.js
@@ -1,6 +1,7 @@
-import './Consoles.css'
+import './Consoles.css';
 
 const Consoles = () => {
+
     const products = [
         {
             img: '../images/game-boy.svg',
@@ -36,30 +37,30 @@ const Consoles = () => {
 
     return(
         <>
-        <section className='container'>
-            <h2>Consoles</h2>
-            <div className='produtos'>
-                <p>Ver tudo </p>
-                <a href='/'>
-                    <img src='../images/vetor.svg' alt='Ver Tudo'></img>
-                </a>
-            </div>
-        </section>
+            <section className='container'>
+                <h2>Consoles</h2>
+                    <div className='produtos'>
+                        <p>Ver tudo </p>
+                            <a href='/'>
+                                <img src='../images/vetor.svg' alt='Ver Tudo'></img>
+                            </a>
+                    </div>
+            </section>
 
-        <section className='container-produtos'>
-            <div className='produtos-individuais'>
-                {products.map((products) => {
-                    return(
-                        <div className='produto'>
-                            <img className='product-image' src={products.img} alt='Produto'></img>
-                            <p className='title'>{products.name}</p>
-                            <p className='price'>{products.price}</p>
-                            <p className='see-all'>Ver produto</p>
-                        </div>
-                    )
-                })}
-            </div>
-        </section>
+            <section className='container-produtos'>
+                <div className='produtos-individuais'>
+                    {products.map((products) => {
+                        return(
+                            <div className='produto'>
+                                <img className='product-image' src={products.img} alt='Produto'></img>
+                                <p className='title'>{products.name}</p>
+                                <p className='price'>{products.price}</p>
+                                <p className='see-all'>Ver produto</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
         </>
     )
 }
