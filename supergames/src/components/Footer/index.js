4fcfa8c711bd836a605404dfa1901b484caa2b8e
Franklin <frkdj25@gmail.com>
2022-10-31 18:37:39 -0300

    feat: adding buttons on footer

--- supergames/src/components/Footer/index.js
+++ supergames/src/components/Footer/index.js
@@ -4,7 +4,12 @@ const Footer = () => {
     return (
         <div className='footer'>
             <img src='../images/logo.svg' alt='Logo'></img>
-
+            <button>Quem somos nós</button>
+            <button>Política de privacidade</button>
+            <button>Programa Fidelidade</button>
+            <button>Nossas lojas</button>
+            <button>Quero ser franqueado</button>
+            <button>Anuncie aqui</button>
         </div>
     )
 };
