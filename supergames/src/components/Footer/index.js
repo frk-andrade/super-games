b40caa83b357852197915167fe086a0fcedc230a
Franklin <frkdj25@gmail.com>
2022-11-04 18:53:05 -0300

    fix: adjusting form

--- supergames/src/components/Footer/index.js
+++ supergames/src/components/Footer/index.js
@@ -10,15 +10,16 @@ const Footer = () => {
             <a>Nossas lojas</a>
             <a>Quero ser franqueado</a>
             <a>Anuncie aqui</a>
-            Fale Conosco
-            <form className='footer-form'>
-                    <label>
-                        Nome:
-                            <input type="text" name="name" />
-                            <input type="textarea" name="textarea" />
-                    </label>
-                        <input type="submit" value="Enviar mensagem" />
+            <div className='footer-form'>
+            <p> Fale Conosco </p>
+            <form >
+                <label>
+                    <input type="text" name="name" placeholder="Nome"/> <br/>       
+                    <input type="textarea" name="textarea" multiline="true" placeholder = "Insira a sua mensagem"/> <br/> 
+                </label>
+                    <input type="submit" value="Enviar mensagem" />
             </form>
+            </div>
         </div>
     )
 };
