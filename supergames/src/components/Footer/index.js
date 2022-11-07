e695027cfb00219961b6be1e9de97434eb21c6b1
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-11-07 19:06:42 -0300

    style footer

--- supergames/src/components/Footer/index.js
+++ supergames/src/components/Footer/index.js
@@ -3,22 +3,33 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className='footer'>
-            <img src='../images/logo.svg' alt='Logo'></img>
-            <a>Quem somos nós</a>
-            <a>Política de privacidade</a>
-            <a>Programa Fidelidade</a>
-            <a>Nossas lojas</a>
-            <a>Quero ser franqueado</a>
-            <a>Anuncie aqui</a>
-            <div className='footer-form'>
-            <p> Fale Conosco </p>
-            <form >
-                <label>
-                    <input type="text" name="name" placeholder="Nome"/> <br/>       
-                    <input type="textarea" name="textarea" multiline="true" placeholder = "Insira a sua mensagem"/> <br/> 
-                </label>
-                    <input type="submit" value="Enviar mensagem" />
-            </form>
+            <div className="content-footer">
+                <div className="container-links">
+                    <img src='../images/logo.svg' alt='Logo'></img>
+                    <div className="links-footer">
+                        <a>Quem somos nós</a>
+                        <a>Política de privacidade</a>
+                        <a>Programa Fidelidade</a>
+                        <a>Nossas lojas</a>
+                        <a>Quero ser franqueado</a>
+                        <a>Anuncie aqui</a>
+                    </div>
+                </div>
+                <div className='footer-form'>
+                    <div className="title-form">
+                        <p> Fale Conosco </p>
+                    </div>
+                    <form className="form">
+                        <div className="input-name">
+                            <input type="text" name="name" placeholder="Nome"/>       
+                        </div>
+                        <div className="input-mensage">
+                            <input type="textarea" name="textarea" multiline="true" placeholder = "Insira a sua mensagem"/> 
+                        </div>
+
+                        <button className="button-form">Enviar Mensagem</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
